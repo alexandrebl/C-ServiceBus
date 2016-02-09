fda83b0502da30923986deca62cd155fc6992552
alexandrebl <alexandre@insidesis.com>
2016-02-09 11:47:17 -0200

    Ajustes na classe principal para enviar 1000 em sequencia

--- ServiceBusSendToQueue/main.cpp
+++ ServiceBusSendToQueue/main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <boost/asio.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
 #include "asiohandler.h"
 
@@ -8,22 +6,35 @@ int main(void)
 {
     boost::asio::io_service ioService;
     AsioHandler handler(ioService);
-    handler.connect("localhost", 5672);
+    
+    int const limit = 1000;
+    
+    std::cout << "Application End!'" << std::endl;
+    
+    handler.connect("127.0.0.1", 5672);
 
     AMQP::Connection connection(&handler, AMQP::Login("guest", "guest"), "/");
     AMQP::Channel channel(&connection);
 
-    boost::asio::deadline_timer t(ioService, boost::posix_time::millisec(100));
+    boost::asio::deadline_timer t(ioService, boost::posix_time::millisec(1000));
     
     channel.onReady([&]()
     {
         if(handler.connected())
         {
-            channel.declareExchange("helloex");
-            channel.publish("helloex", "hello", "Hello World!");
-            std::cout << " [x] Sent 'Hello World!'" << std::endl;
+            channel.declareQueue("FirstQueue");
+            
+            std::cout << "Initializing to sent " << limit << " messages" << std::endl;
+            
+            for(int x = 0; x < 1000; x++){
+                channel.publish("", "FirstQueue", "Hello World!");
+            }
+            
+            std::cout << "Messages has been processed" << std::endl;
 
             t.async_wait([&](const boost::system::error_code&){ioService.stop();});
+            
+            std::cout << "Application End!'" << std::endl;
         }
     });
 
